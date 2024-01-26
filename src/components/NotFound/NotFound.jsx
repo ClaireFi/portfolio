e88352267692435eaefd3e446e2c33350de1427a
cfichter <claire.fichter@sequoiasoft.com>
2024-01-26 22:20:34 +0100

    Ajout texte Page 404

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,10 +1,20 @@
-import {Link} from "react-router-dom"
+import Container from "../_Layout/Container/Container";
+import { Link } from "react-router-dom";
 
 export default function NotFound() {
   return (
-    <div>
-      <h1>Page not found</h1>
-      <button className="border"><Link to="/">Retour</Link></button>
+    <div className="mx-auto max-w-7xl p-4 sm:p-6 lg:p-14 top-[3em]">
+      <Container>
+        <h1>404 : Page not found. </h1>
+        <div>
+          404 : Qui aurait cru que même dans un portfolio bien organisé, une
+          page puisse jouer à cache-cache ? On la cherche activement, mais en
+          attendant, pourquoi ne pas découvrir les autres projets qui restent bien sagement à leur place ?
+        </div>
+        <button className="border button">
+          <Link to="/#projects">Retour</Link>
+        </button>
+      </Container>
     </div>
   );
 }
