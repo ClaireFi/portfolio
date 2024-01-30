2b28190d76cc9f60e6c0a639fe33b831c5f10369
cfichter <claire.fichter@sequoiasoft.com>
2024-01-30 16:25:03 +0100

    Font primary ajoutée

--- src/components/_Layout/Content/Content.jsx
+++ src/components/_Layout/Content/Content.jsx
@@ -14,7 +14,7 @@ export default function Content() {
   const { toggleTheme, darkMode } = useContext(ThemeContext);
 
   return (
-    <main id="main">
+    <main id="main" className="font-primary">
 
       <Presentation />
       <div className="relative top-[100vh] z-10 bg-white dark:bg-[#111] [transition-property:background-color] duration-500 ease-[ease] delay-[0ms]">
